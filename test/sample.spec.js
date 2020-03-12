ec7022f2e6b81b7a2b56027bbc9dbc99ce01743d
brylledesigns <clarido.christianbryllevon@gmail.com>
2020-03-12 15:49:12 +0800

    first commit

--- test/sample.spec.js
+++ test/sample.spec.js
@@ -3,7 +3,7 @@ describe('simple test for fv main console', async  () => {
     
     before(async () => {
         page = await browser.newPage();
-        await page.goto("https://www.linkedin.com/");
+        await page.goto("https://www.linkedin.com/login?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin");
         await page.setViewport( { width: 1920, height: 1040} );
     });
 
@@ -12,9 +12,9 @@ describe('simple test for fv main console', async  () => {
     });
 
     it('should login to home page', async () => {
-        const emailInput = "#login-email";
-        const passwordInput = "#login-password";
-        const submitSelector = "#login-submit";
+        const emailInput = "#username";
+        const passwordInput = "#password";
+        const submitSelector = "#app__container > main > div > form > div.login__form_action_container > button";
          
          linkEmail = await page.$(emailInput);
          linkPassword = await page.$(passwordInput);
